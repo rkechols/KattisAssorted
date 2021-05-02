583ddc83eca7b199770df9db0d42c43448c6403d
Ryan Echols <rkechols21@gmail.com>
2021-05-02 02:45:02 -0600

    added notes about thinking their answer is wrong

--- prva_test.py
+++ prva_test.py
@@ -4,9 +4,9 @@ from prva import prva
 if __name__ == "__main__":
 	all_passing = True
 	for in_values, answer in [
-		(["luka", "o#a#", "kula", "i#a#"], "kala"),
+		(["luka", "o#a#", "kula", "i#a#"], "kala"),  # multiple answers?
 		(["luka", "o#a#", "kula", "i#as"], "as"),
-		(["adaca", "da##b", "abb#b", "abbac"], "abb")
+		(["adaca", "da##b", "abb#b", "abbac"], "abb")  # wrong answer? what about "da"?
 	]:
 		out = prva(in_values)
 		if out != answer:
